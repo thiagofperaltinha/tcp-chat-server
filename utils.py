262f234447fcbe4fcbee3cc205467dedd8bb6555
Thiago_Fernandes_Lopes_S <165108906+thiagofperaltinha@users.noreply.github.com>
2025-04-22 20:18:27 -0300

    adicionando método de verificação pro nome de usuario

--- utils.py
+++ utils.py
@@ -1,26 +1,33 @@
 import threading
+
 print_lock = threading.Lock()
 encerrando = False
- 
-def verificar_msg_recebida(mensagem_recebida):
-    if mensagem_recebida == "exit chat":
-        return True
-    return False
+nome_clientes = []
+nomes_proibidos = ["", "Server".lower()]
 
+# Verifica se o cliente enviou o comando para encerrar o chat
+def verificar_msg_recebida(mensagem_recebida):
+    return mensagem_recebida.strip().lower() == "exit chat"
+
+# Verifica se o servidor quer encerrar e confirma com o usuário
 def verificar_msg_enviada(s, mensagem_enviada):
-    if mensagem_enviada == "exit chat":
-        global encerrando
-        verifi = input("Are you sure you want to end this chat? To confirm type: <yes> ")
+    global encerrando
+    if mensagem_enviada.strip().lower() == "exit chat":
+        verifi = input("Are you sure you want to end this chat? To confirm type: <yes> ").strip().lower()
         if verifi == "yes":
             encerrando = True
             print("Connection closed.")
-            s.send(mensagem_enviada.encode())
+            try:
+                s.send(mensagem_enviada.encode())
+            except:
+                pass
             return True
         else:
             print("Exit cancelled. Continuing chat...")
             return False
     return False
 
+# Thread responsável por receber mensagens do servidor
 def receber_mensagens(s):
     while True:
         try:
@@ -28,15 +35,44 @@ def receber_mensagens(s):
             if verificar_msg_recebida(mensagem_recebida):
                 break
             with print_lock:
+                
                 print(f"\nServer: {mensagem_recebida}")
                 print("You: ", end="", flush=True)
-                
-            if mensagem_recebida.lower() == "Service":
-                s.send("You have accessed a chat server. Soon, new clients will be added to interact with you!".encode())
         except:
             if not encerrando:
                 print("Error receiving message.")
             break
+# Método responsável por exibir as mensagens automaticas correspondentes aos comandos utilizados no servidor
+def comandos(mensagem_recebida, s):
+    msg = mensagem_recebida.lower()
+
+    if msg == "<Service>":
+        resposta = "You have accessed a chat server. Soon, new clients will be added to interact with you!"
+        try:
+            s.send(resposta.encode())
+        except:
+            print("Erro ao exibir mensagem de Serviços")
+            
+    elif msg == "<Help>":
+        with print_lock:
+            print("\n📄 Available commands:")
+            print("  - exit chat : ends the conversation.")
+            print("  - Service   : see the current status of the chat server.")
+            print("  - Help      : show this help message.\n")
+            
+            
+def user_name (nome_usuario, nomes_proibidos, nome_clientes):
+        if (nome_usuario in nomes_proibidos):
+            novo_nome = input("Digite um nome de usuário válido: ")
+            return user_name(novo_nome, nomes_proibidos, nome_clientes)
+        elif nome_usuario in nome_clientes:
+            print("Nome em uso, tente novamente.")
+            novo_nome = input("Digite um nome de usuário válido: ")
+            return user_name(novo_nome, nomes_proibidos, nome_clientes)
+        else:
+            nome_clientes.append(nome_usuario)
+            return nome_usuario
+           
         
 
 # If client requests services, show a service message
