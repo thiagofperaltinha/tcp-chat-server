8fc52742f571e7009582967748638105a72bd5be
Thiago_Fernandes_Lopes_S <165108906+thiagofperaltinha@users.noreply.github.com>
2025-05-04 19:38:17 -0300

    comentários e explicação adicionados em todos os arquivos

--- utils/cont_bytes.py
+++ utils/cont_bytes.py
@@ -1,19 +1,29 @@
 def enviar_dados_tamanho(dados, s):
+    # Convert the length of the data to 4 bytes in big-endian order
     tamanho = len(dados).to_bytes(4, byteorder="big")
+    # Send the length followed by the actual data
     s.sendall(tamanho + dados)
     
 def receber_dados_tamanho(s):
+    # Receive the first 4 bytes which contain the size of the data
     tamanho_bytes = s.recv(4)
     
+    # If less than 4 bytes are received, raise an error
     if len(tamanho_bytes) < 4:
-        raise ValueError("ERRO DE VALOR")
+        raise ValueError("VALUE ERROR")
     
+    # Convert the 4 bytes to an integer to get the data size
     tamanho = int.from_bytes(tamanho_bytes, byteorder="big")
-    dado = b''
-    while len(dado)<tamanho:
-        res = s.recv(tamanho - len(dado))
+    
+    dado = b''  # Initialize an empty byte object to hold the received data
+    
+    # Keep receiving data until the full size is received
+    while len(dado) < tamanho:
+        res = s.recv(tamanho - len(dado))  # Receive remaining data
         if not res:
-            raise ValueError("ERRO DE VALOR")
-        
-        dado += res
+            # If no data is received, raise an error
+            raise ValueError("VALUE ERROR")
+        dado += res  # Add the received data to the 'dado' variable
+    
+    # Return the complete data
     return dado
--- utils/log_chat.py
+++ utils/log_chat.py
@@ -1,5 +1,5 @@
 import datetime
-
+# Function chat log
 def chat_log(username, mensagem):
     with open("chat.txt", "a") as f:
         data = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
