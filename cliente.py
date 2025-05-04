12f2e4c1d1a6edcefa1e8dc189642fa1411f69f0
Thiago_Fernandes_Lopes_S <165108906+thiagofperaltinha@users.noreply.github.com>
2025-05-04 19:37:11 -0300

    refactor(cl): verificações de erro melhoradas

--- cliente.py
+++ cliente.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import base64
 from u import verificar_msg_enviada, receber_mensagens, user_name, clientes_info, nomes_proibidos
 from utils.crypto import cripto_mesage, serialize_public_key, deserialize_public_key, gerar_chave
 from utils.log_chat import chat_log
@@ -13,27 +14,29 @@ port = 3000
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((ip, port))
 
+# Generate public and private keys
 public_key, private_key = gerar_chave()
+# Send the public key to the server
 enviar_dados_tamanho(serialize_public_key(public_key), s)
 
 try:
-    # Receber a chave pública do servidor
+    # Receive the server's public key
     receber_tam_men = receber_dados_tamanho(s)
     server_key = deserialize_public_key(receber_tam_men)
 except Exception as e:
-    print(f"❌ Erro ao receber a chave pública do servidor: {e}")
+    print(f"❌ Error receiving the server's public key: {e}")
     s.close()
     exit(1)
 
 # Welcome message from the server
 welcome_message = s.recv(1024).decode()
-print("╭────────────────────────────────────────────╮")
+print("╭───────────────────────────────────────────────────╮")
 print(f"│ 📡 Server says: {welcome_message.strip()}")
-print("╰────────────────────────────────────────────╯\n")
+print("╰───────────────────────────────────────────────────╯\n")
 
 # Instructions for the user
 print("╭───────────────────────────────────────────────────────╮")
-print("│ 📌 Type <help> to learn how to use the chat commands.")
+print("│ 📌 Type help* to learn how to use the chat commands.")
 print("╰───────────────────────────────────────────────────────╯\n")
 
 # Thread to receive messages from the server
@@ -48,13 +51,18 @@ booleano = True
 # Main message sending loop
 while True:
     try:
+        # Take user input for the message
         mensagem_enviada = input()
+        # Verify if the message is valid to send
         msg_env = verificar_msg_enviada(s, mensagem_enviada, server_key)
         if msg_env:
             break
             
+        # Encrypt the message
         mensagem_criptografada = cripto_mesage(mensagem_enviada, server_key)
+        # Send the encrypted message to the server
         enviar_dados_tamanho(mensagem_criptografada, s)
+        # Log the sent message
         chat_log(verifi_nome, mensagem_enviada)
     except EOFError:
         break
